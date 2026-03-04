06cf1c44be3904cf95e1ad93c78da7c541db58a6
Marcos Vinicius <marcos_araujo7@hotmail.com>
2026-03-04 17:38:13 -0300

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
+  rules: {
+    'comma-dangle': 'off',
+    'function-paren-newline': 'off',
+    'prettier/prettier': 'off',
+  },
+};
